e7a345fccb72c9d4ce4a8a88194b1927778c9ec2
kdkdjdjd <116704003+kdkdjdjd@users.noreply.github.com>
2023-04-09 20:20:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,40 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T arr[size] = {0};
+    int a;
+
+ public:
+    TStack() {
+        a = -1;
+    }
+    T get() const {
+        return arr[a];
+    }
+    T pop() {
+        return arr[a--];
+    }
+    int pri() const {
+        return a;
+    }
+    bool isEmpty() const {
+        return a == -1;
+    }
+    bool isFull() const {
+        return a == size;
+    }
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::string(" empty");
+        } else {
+            arr[++a] = value;
+        }
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
